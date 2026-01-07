048ae7f68a4d24a99b44e9cdec9fe5ec27f213b2
Dqnial <danial_20000@mail.ru>
2026-01-07 20:35:02 +0500

    fix for deploy

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -10,8 +10,8 @@ const generateToken = (res: Response, userId: string): void => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
