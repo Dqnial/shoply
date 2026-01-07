336d8e881ac6370ea95165fcb861ebc77841103a
Dqnial <danial_20000@mail.ru>
2026-01-07 20:01:59 +0500

    fix: added import process

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import process from "node:process";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI!);
-        console.log('MongoDB Connected');
-    } catch (err) {
-        console.error('Error connecting to MongoDB', err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("Error connecting to MongoDB", err);
+    process.exit(1);
+  }
 };
